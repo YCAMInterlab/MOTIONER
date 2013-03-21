cc9386ad2a528fbe157fd62e3442e29ae9297af5
Onishi Yoshito <g6136t@gmail.com>
2013-03-21 18:02:07 +0100

    updated submodules.sh

--- submodules.sh
+++ submodules.sh
@@ -17,10 +17,11 @@ if [ -r .git ]; then
 else
 
 	# clone from repo
-	cd ./addons
+	cd ./oF/MOTIONER/src/addons
 
-	echo "[couldn't found ./.gitmodule. start clone five addons from github repos instead of git submodule update]"
+	echo "[couldn't found ./.gitmodule. start clone seven addons from github repos instead of git submodule update]"
 
+	git clone https://github.com/rezaali/ofxUI.git
 	git clone https://github.com/YoshitoONISHI/ofxException.git
 	git clone https://github.com/YoshitoONISHI/ofxNonCopyable.git
 	git clone https://github.com/YoshitoONISHI/ofxEvent.git
@@ -29,5 +30,5 @@ else
 	git clone https://github.com/YoshitoONISHI/ofxGLUtils.git
 
 	echo "end clone addons"
-	cd ../
+	cd ../../../../
 fi
